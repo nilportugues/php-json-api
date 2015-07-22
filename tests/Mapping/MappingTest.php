2a78a9483093e542c1559f6a158198f4ec0ec31f
Nil Portugués Calderó <nilopc@gmail.com>
2015-07-23 01:09:34 +0200

    Mapping testing..

---
+++ tests/Mapping/MappingTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Author: Nil Portugués Calderó <contact@nilportugues.com>
+ * Date: 7/22/15
+ * Time: 11:57 PM
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NilPortugues\Tests\Api\Mapping;
+
+
+class MappingTest extends \PHPUnit_Framework_TestCase {
+
+}
+ 
