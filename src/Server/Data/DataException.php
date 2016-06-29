ce2b8cede67a1ae90e75904ec7b140fa11fada27
Clemens John <clemens.john@floh1111.de>
2016-06-29 11:16:40 +0200

    Add ability to add ErrorBag to DataException, references nilportugues/laravel5-jsonapi#54
    Signed-off-by: Clemens John <clemens.john@floh1111.de>

--- src/Server/Data/DataException.php
+++ src/Server/Data/DataException.php
@@ -10,9 +10,58 @@
 
 namespace NilPortugues\Api\JsonApi\Server\Data;
 
+use NilPortugues\Api\JsonApi\Server\Errors\ErrorBag;
+
 /**
  * Class DataException.
  */
 class DataException extends \InvalidArgumentException
 {
+  /**
+   * MessageBag errors.
+   *
+   * @var \NilPortugues\Api\JsonApi\Server\Errors\ErrorBag
+   */
+  protected $errors;
+
+  /**
+   * Create a new data exception instance.
+   *
+   * @param string                                                 $message
+   * @param \NilPortugues\Api\JsonApi\Server\Errors\ErrorBag|array $errors
+   * @param \Exception                                             $previous
+   * @param array                                                  $headers
+   * @param int                                                    $code
+   *
+   * @return void
+   */
+  public function __construct($message = null, $errors = null, $code = 0, Exception $previous = null)
+  {
+    if (is_null($errors)) {
+      $this->errors = new ErrorBag;
+    } else {
+      $this->errors = is_array($errors) ? new ErrorBag($errors) : $errors;
+    }
+    parent::__construct($message, $code, $previous);
+  }
+
+  /**
+   * Get the errors message bag.
+   *
+   * @return \Illuminate\Support\MessageBag
+   */
+  public function getErrors()
+  {
+    return $this->errors;
+  }
+
+  /**
+   * Determine if message bag has any errors.
+   *
+   * @return bool
+   */
+  public function hasErrors()
+  {
+    return (bool)$this->errors->count();
+  }
 }
