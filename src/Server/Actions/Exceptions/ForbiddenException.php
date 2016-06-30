b4295a1b2d5cf55f60ca3faca7bf4d0127463365
Clemens John <clemens.john@floh1111.de>
2016-06-30 16:03:29 +0200

    Fix formatting
    Signed-off-by: Clemens John <clemens.john@floh1111.de>

--- src/Server/Actions/Exceptions/ForbiddenException.php
+++ src/Server/Actions/Exceptions/ForbiddenException.php
@@ -12,7 +12,7 @@ class ForbiddenException extends \Exception
      * @param string $message
      * @param Exception $previous
      */
-    public function __construct($message, $previous=null)
+    public function __construct($message, $previous = null)
     {
         parent::__construct($message, 403, $previous);
     }
@@ -20,11 +20,13 @@ class ForbiddenException extends \Exception
     /**
      * @param string $title
      */
-    public function setTitle($title) {
+    public function setTitle($title)
+    {
         $this->title = $title;
     }
 
-    public function getTitle() {
+    public function getTitle()
+    {
         return $this->title;
     }
 }
