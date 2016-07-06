a298ec918dfee4df795a6dda7acaa741f05dfdbe
Francisco Benza <franbenz@gmail.com>
2016-07-06 17:53:07 -0400

    Testing the sqlite database on memory.

--- tests/Doctrine/DoctrineTest.php
+++ tests/Doctrine/DoctrineTest.php
@@ -7,7 +7,17 @@ require_once 'AbstractTestCase.php';
 // use NilPortugues\Tests\JsonApi\Doctrine\AbstractTestCase;
 class DoctrineTest extends AbstractTestCase {
 
-	public function testDummy() {
-		$this->assertEquals(1, 1);
+	public function testPersit() {
+		$newCustomer = new \Doctrine\Entity\Customer();
+		$newCustomer->setActive(true);
+		$newCustomer->setName('Name 1');
+		
+		$this->entityManager->persist($newCustomer);
+		$this->entityManager->flush();
+		$repoCustomer = $this->entityManager->getRepository('Doctrine\\Entity\\Customer');
+		$savedCustomer = $repoCustomer->findAll();
+		
+		$this->assertEquals($newCustomer->getId(), $savedCustomer[0]->getId());
+		$this->assertEquals($newCustomer->getName(), $savedCustomer[0]->getName());
 	}
 }
