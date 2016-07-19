0aefd992ce49b4b60e284fcc1f51b94b6163e896
Rodrigo Ruiz Diaz <rruizdiaz@roshka.com>
2016-07-19 11:49:30 -0400

    Added testSecondLevelNestingEntity. A comment refers to another comment. Moved test data insertion and mappings to setUpBeforeClass.

--- tests/Integrations/Doctrine/AbstractTestCase.php
+++ tests/Integrations/Doctrine/AbstractTestCase.php
@@ -8,6 +8,12 @@ namespace NilPortugues\Tests\Api\JsonApi\Integrations\Doctrine;
 
 use Doctrine\DBAL\Schema\SchemaException;
 use Doctrine\ORM\Tools\SchemaTool;
+use NilPortugues\Tests\Api\JsonApi\Integrations\Doctrine\Entity\Customer;
+use NilPortugues\Tests\Api\JsonApi\Integrations\Doctrine\Mappings\CustomerMapping;
+use NilPortugues\Tests\Api\JsonApi\Integrations\Doctrine\Mappings\PostMapping;
+use NilPortugues\Tests\Api\JsonApi\Integrations\Doctrine\Mappings\CommentMapping;
+use NilPortugues\Tests\Api\JsonApi\Integrations\Doctrine\Entity\Post;
+use NilPortugues\Tests\Api\JsonApi\Integrations\Doctrine\Entity\Comment;
 
 require_once 'bootstrap.php';
 
@@ -17,12 +23,43 @@ abstract class AbstractTestCase extends \PHPUnit_Framework_TestCase
      * @var \Doctrine\ORM\EntityManager
      */
     protected static $entityManager;
+    protected static $classConfig;
 
     public static function setUpBeforeClass()
     {
         self::$entityManager = GetEntityManager();
         // Build the schema for sqlite
         self::generateSchema();
+
+        $newCustomer = new Customer();
+        $newCustomer->setActive(true);
+        $newCustomer->setName('Name 1');
+        self::$entityManager->persist($newCustomer);
+
+        $newPost = new Post();
+        $newPost->setCustomer($newCustomer);
+        $newPost->setDate(new \DateTime('2016-07-12 16:30:12.000000'));
+        $newPost->setDescription('Description test');
+        self::$entityManager->persist($newPost);
+
+        $newComment = new Comment();
+        $newComment->setPost($newPost);
+        $newComment->setComment('Comment 1');
+        self::$entityManager->persist($newComment);
+
+        $newComment2 = new Comment();
+        $newComment2->setPost($newPost);
+        $newComment2->setComment('Comment 2');
+        $newComment2->setParentComment($newComment);
+        self::$entityManager->persist($newComment2);
+
+        self::$entityManager->flush();
+
+        self::$classConfig = [
+                CustomerMapping::class,
+                PostMapping::class,
+                CommentMapping::class,
+        ];
 
         parent::setUpBeforeClass();
     }
--- tests/Integrations/Doctrine/DoctrineTest.php
+++ tests/Integrations/Doctrine/DoctrineTest.php
@@ -6,158 +6,266 @@ use NilPortugues\Api\JsonApi\JsonApiSerializer;
 use NilPortugues\Api\JsonApi\JsonApiTransformer;
 use NilPortugues\Api\Mapping\Mapper;
 use NilPortugues\Tests\Api\JsonApi\Integrations\Doctrine\Entity\Customer;
-use NilPortugues\Tests\Api\JsonApi\Integrations\Doctrine\Mappings\CustomerMapping;
-use NilPortugues\Tests\Api\JsonApi\Integrations\Doctrine\Mappings\PostMapping;
-use NilPortugues\Tests\Api\JsonApi\Integrations\Doctrine\Mappings\CommentMapping;
 use NilPortugues\Tests\Api\JsonApi\Integrations\Doctrine\Entity\Post;
 use NilPortugues\Tests\Api\JsonApi\Integrations\Doctrine\Entity\Comment;
 
 class DoctrineTest extends AbstractTestCase
 {
-    public function testPersistAndSerializeSimpleEntity()
+    public function testSerializeSimpleEntity()
     {
-        $newCustomer = new Customer();
-        $newCustomer->setActive(true);
-        $newCustomer->setName('Name 1');
-
-        self::$entityManager->persist($newCustomer);
-        self::$entityManager->flush();
         $repoCustomer = self::$entityManager->getRepository(Customer::class);
-        $savedCustomer = $repoCustomer->findAll();
-
-        $classConfig = [
-            CustomerMapping::class,
-        ];
+        $savedCustomer = $repoCustomer->find(1);
 
         $expected = <<<JSON
-			{
-				"data":
-					[{
-						"type":"customer",
-						"id":"1",
-						"attributes":
-							{
-								"active":true,
-								"id":1,
-								"name":"Name 1"
-							},
-						"links":
-							{
-								"self":{"href":"http://example.com/customer/1"}
-							}
-					}],
-				"jsonapi":
-					{
-						"version":"1.0"
-					}
-			}
+		{  
+		   "data":{  
+		      "type":"customer",
+		      "id":"1",
+		      "attributes":{  
+		         "active":true,
+		         "id":1,
+		         "name":"Name 1"
+		      },
+		      "links":{  
+		         "self":{  
+		            "href":"http://example.com/customer/1"
+		         }
+		      }
+		   },
+		   "links":{  
+		      "self":{  
+		         "href":"http://example.com/customer/1"
+		      }
+		   },
+		   "jsonapi":{  
+		      "version":"1.0"
+		   }
+		}
 JSON;
-        $mapper = new Mapper($classConfig);
+        $mapper = new Mapper(self::$classConfig);
         $transformer = new JsonApiTransformer($mapper);
         $serializer = new JsonApiSerializer($transformer);
         $customerSerialize = $serializer->serialize($savedCustomer);
 
-        $this->assertEquals($newCustomer->getId(), $savedCustomer[0]->getId());
-        $this->assertEquals($newCustomer->getName(), $savedCustomer[0]->getName());
         $this->assertEquals(json_decode($expected, true), json_decode($customerSerialize, true));
     }
 
-    public function testPersistAndSerializeComplexEntity()
+    public function testSerializeComplexEntity()
     {
-        $newCustomer = new Customer();
-        $newCustomer->setActive(true);
-        $newCustomer->setName('Name 1');
-
-        self::$entityManager->persist($newCustomer);
-        self::$entityManager->flush();
-
-        $newPost = new Post();
-        $newPost->setCustomer($newCustomer);
-        $newPost->setDate(new \DateTime('2016-07-12 16:30:12.000000'));
-        $newPost->setDescription('Description test');
-        self::$entityManager->persist($newPost);
-        self::$entityManager->flush();
-
-        $newComment = new Comment();
-        $newComment->setPost($newPost);
-        $newComment->setComment('Comment 1');
-        self::$entityManager->persist($newComment);
-        self::$entityManager->flush();
-
         $repoCustomer = self::$entityManager->getRepository(Comment::class);
-        $savedComment = $repoCustomer->findAll();
-
-        $classConfig = [
-                CustomerMapping::class,
-                PostMapping::class,
-                CommentMapping::class,
-        ];
+        $savedComment = $repoCustomer->find(1);
 
         $expected = <<<JSON
-        {
-        		"data":
-		[{
-			"type":"comment",
-			"id":"1",
-			"attributes":
-				{
-					"comment":"Comment 1",
-					"id":1,
-					"parent_comment":null,
-					"parent_id":null
-				},
-			"links":
-				{"self":{"href":"http://example.com/comment/1"}},
-			"relationships":
-				{
-					"post":
-						{
-							"data":
-								{
-									"type":"post",
-									"id":"1"
-								}
-						}
-				}
+        {  
+		   "data":{  
+		      "type":"comment",
+		      "id":"1",
+		      "attributes":{  
+		         "comment":"Comment 1",
+		         "id":1,
+		         "parent_comment":null,
+		         "parent_id":null
+		      },
+		      "links":{  
+		         "self":{  
+		            "href":"http://example.com/comment/1"
+		         }
+		      },
+		      "relationships":{  
+		         "post":{  
+		            "data":{  
+		               "type":"post",
+		               "id":"1"
+		            }
+		         }
+		      }
+		   },
+		   "included":[  
+		      {  
+		         "type":"customer",
+		         "id":"1",
+		         "attributes":{  
+		            "name":"Name 1",
+		            "active":true
+		         },
+		         "links":{  
+		            "self":{  
+		               "href":"http://example.com/customer/1"
+		            }
+		         }
+		      },
+		      {  
+		         "type":"post",
+		         "id":"1",
+		         "attributes":{  
+		            "date":{  
+		               "date":"2016-07-12 16:30:12.000000",
+		               "timezone_type":3,
+		               "timezone":"Europe/Madrid"
+		            },
+		            "description":"Description test"
+		         },
+		         "relationships":{  
+		            "customer":{  
+		               "data":{  
+		                  "type":"customer",
+		                  "id":"1"
+		               }
+		            }
+		         },
+		         "links":{  
+		            "self":{  
+		               "href":"http://example.com/post/1"
+		            }
+		         }
+		      }
+		   ],
+		   "links":{  
+		      "self":{  
+		         "href":"http://example.com/comment/1"
+		      }
+		   },
+		   "jsonapi":{  
+		      "version":"1.0"
+		   }
 		}
-	],
-			"included":
-				[
-					{
-						"type":"customer",
-						"id":"2",
-						"attributes":
-							{
-								"name":"Name 1",
-								"active":true
-							},
-						"links":{"self":{"href":"http://example.com/customer/2"}}
-					},
-					{	"type":"post",
-						"id":"1",
-						"attributes":
-							{
-								"date":{"date":"2016-07-12 16:30:12.000000","timezone_type":3,"timezone":"Europe/Madrid"},
-								"description":"Description test"
-							},
-						"relationships":
-							{
-								"customer":
-									{
-										"data":
-											{
-												"type":"customer",
-												"id":"2"
-											}
-									}
-							},
-						"links":{"self":{"href":"http://example.com/post/1"}}
-					}
-				],
-			"jsonapi":{"version":"1.0"}
-}
 JSON;
-        $mapper = new Mapper($classConfig);
+        $mapper = new Mapper(self::$classConfig);
+        $transformer = new JsonApiTransformer($mapper);
+        $serializer = new JsonApiSerializer($transformer);
+        $commentSerialize = $serializer->serialize($savedComment);
+
+        $this->assertEquals(json_decode($expected, true), json_decode($commentSerialize, true));
+    }
+
+    public function testSecondLevelNestingEntitySerialize()
+    {
+        $repoCustomer = self::$entityManager->getRepository(Comment::class);
+        $savedComment = $repoCustomer->find(2);
+
+        $expected = <<<JSON
+        {  
+		   "data":{  
+		      "type":"comment",
+		      "id":"2",
+		      "attributes":{  
+		         "comment":"Comment 2",
+		         "id":2,
+		         "parent_id":null
+		      },
+		      "links":{  
+		         "self":{  
+		            "href":"http://example.com/comment/2"
+		         }
+		      },
+		      "relationships":{  
+		         "parentComment":{  
+		            "data":{  
+		               "type":"comment",
+		               "id":"1"
+		            }
+		         },
+		         "post":{  
+		            "data":{  
+		               "type":"post",
+		               "id":"1"
+		            }
+		         }
+		      }
+		   },
+		   "included":[  
+		      {  
+		         "type":"customer",
+		         "id":"1",
+		         "attributes":{  
+		            "name":"Name 1",
+		            "active":true
+		         },
+		         "links":{  
+		            "self":{  
+		               "href":"http://example.com/customer/1"
+		            }
+		         }
+		      },
+		      {  
+		         "type":"post",
+		         "id":"1",
+		         "attributes":{  
+		            "description":"Description test"
+		         },
+		         "relationships":{  
+		            "customer":{  
+		               "data":{  
+		                  "type":"customer",
+		                  "id":"1"
+		               }
+		            }
+		         },
+		         "links":{  
+		            "self":{  
+		               "href":"http://example.com/post/1"
+		            }
+		         }
+		      },
+		      {  
+		         "type":"comment",
+		         "id":"1",
+		         "attributes":{  
+		            "comment":"Comment 1",
+		            "parent_id":null,
+		            "parentComment":null
+		         },
+		         "relationships":{  
+		            "post":{  
+		               "data":{  
+		                  "type":"post",
+		                  "id":"1"
+		               }
+		            }
+		         },
+		         "links":{  
+		            "self":{  
+		               "href":"http://example.com/comment/1"
+		            }
+		         }
+		      },
+		      {  
+		         "type":"post",
+		         "id":"1",
+		         "attributes":{  
+		            "date":{  
+		               "date":"2016-07-12 16:30:12.000000",
+		               "timezone_type":3,
+		               "timezone":"Europe/Madrid"
+		            },
+		            "description":"Description test"
+		         },
+		         "relationships":{  
+		            "customer":{  
+		               "data":{  
+		                  "type":"customer",
+		                  "id":"1"
+		               }
+		            }
+		         },
+		         "links":{  
+		            "self":{  
+		               "href":"http://example.com/post/1"
+		            }
+		         }
+		      }
+		   ],
+		   "links":{  
+		      "self":{  
+		         "href":"http://example.com/comment/2"
+		      }
+		   },
+		   "jsonapi":{  
+		      "version":"1.0"
+		   }
+		}
+JSON;
+        $mapper = new Mapper(self::$classConfig);
         $transformer = new JsonApiTransformer($mapper);
         $serializer = new JsonApiSerializer($transformer);
         $customerSerialize = $serializer->serialize($savedComment);
